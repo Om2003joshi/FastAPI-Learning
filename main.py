a870d84be6020f397b33b01344532d8f7f468291
om2003joshi <om2003joshi@gmail.com>
2025-09-22 16:29:09 +0530

    second commit is to get fun and load data set

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patient.json','r') as f :
+        data = json.load(f)
+
+    return data    
 
 @app.get("/")
 def read_root():
@@ -9,3 +16,8 @@ def read_root():
 @app.get('/about')
 def about():
     return{"message":"hello i am om joshi, learning fast api"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
