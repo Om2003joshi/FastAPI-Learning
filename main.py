44c29bd332d501640873bf91e34f18675a34f3b9
om2003joshi <om2003joshi@gmail.com>
2025-09-23 14:04:34 +0530

    makeing asc and desc

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path,HTTPException,Query
 import json
 
 app = FastAPI()
@@ -21,3 +21,27 @@ def about():
 def view():
     data = load_data()
     return data
+
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str = Path(..., description="The ID of the patient in DB",example="P001")):
+    # load all patitent_id
+    data=load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail="Patient not found")    
+
+@app.get('/sort')
+def sort_patient(sort_by:str=Query(...,description="sort on the bbasic od height,weight,and bmi"),order:str=Query('asc',description='sort in asc or descding order')):
+    valid_fields = ['height', 'weight', 'bmi']
+                    
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f"Invalid sort_by field. Must be one of {valid_fields}") 
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail="Invalid order. Must be 'asc' or 'desc'")
+
+    data = load_data()
+    sort_order= True if order=='desc' else False
+    sorted_data = sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=False)    
